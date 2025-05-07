c7c1f865b1c37fda82cccc20c94bcf723db6d5f9
GabrielChiorcea() <107279421+GabrielChiorcea@users.noreply.github.com>
2025-05-07 23:10:40 +0300

    Delete src directory

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import jokeSlice from './jocke-slice';
-
-const store = configureStore({
-  reducer: { jo: jokeSlice.reducer },
-});
-
-export default store;
